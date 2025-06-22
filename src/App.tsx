d7116959f60daa28fafa6ceae9c0fa5688dfe07b
MostakemHossain <mostakememon123@gmail.com>
2025-06-22 20:09:09 +0600

    hero section

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import AboutSection from "./components/About-Section";
 import ContactSection from "./components/Contact-section";
 import Footer from "./components/Footer";
+import HeroSection from "./components/Hero-Section";
 import Navbar from "./components/Navbar";
 import PricingTable from "./components/Pricing-table";
 
@@ -12,6 +13,7 @@ function App() {
 
       {/* Main content goes here */}
       <main className="flex-1">
+        <HeroSection/>
         <AboutSection/>
         <PricingTable />
         <ContactSection />
